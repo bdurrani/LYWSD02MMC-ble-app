e1d7c2864a2ddcd6068f511c6cac8ab016c19fb3
Bilal Durrani <bilal.durrani@xero.com>
2023-01-08 15:51:22 -0500

    more clean up

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 import { LYWSD02 } from "./lywsd02";
 
-const SERVICE_UUID = "ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6";
-const TIME_CHARACTERISTIC_UUID = "ebe0ccb7-7a0a-4b0c-8a1a-6ff2997da3a6";
-
 const statusElement = document.getElementById("status");
 if (statusElement === null) {
   throw new Error("no status element found");
 }
 
-const logElement = document.querySelector("#log");
+const logElement = document.getElementById("log");
 if (logElement === null) {
   throw new Error("no status element found");
 }
 
-function logInput(input: any) {
-  logInfo(input);
+function logMessage(input: any) {
+  logToOutput(input);
   console.log(input);
 }
 
-function logInfo(...args: any[]) {
+function logToOutput(...args: any[]) {
   const line = Array.prototype.slice
     .call(args)
     .map(function (argument) {
@@ -30,43 +27,31 @@ function logInfo(...args: any[]) {
 }
 
 async function setCurrentTime() {
-  console.log("set current time");
+  logMessage("Setting urrent time and timezone");
   const bleDevice = new LYWSD02();
   try {
     await bleDevice.requestDevice();
     await bleDevice.setTime();
-    console.log("Time set");
+    logMessage("Time and timezone set");
   } catch (error) {
-    console.error(`Error setting time ${error}`);
+    logMessage(`Error setting time ${error}`);
   } finally {
+    bleDevice.cleanup();
   }
 }
-
-(async () => {
-  const button = document.getElementById("ble-button");
-
-  const setTimeButton = document.getElementById("set-time-button");
-  setTimeButton?.addEventListener("click", () => setCurrentTime());
-
-  button?.addEventListener("click", async () => {
+async function queryDevice() {
+  {
     const bleDevice = new LYWSD02();
 
     try {
-      statusElement.textContent = "Requesting device...";
+      logMessage("Connecting to device...");
       await bleDevice.requestDevice();
-      statusElement.textContent = "Getting service...";
-      // await bleDevice.queryHistory();
-      // return;
+      logMessage("Querying for current time and time zone service...");
       const currentTime = await bleDevice.getCurrentTime();
 
       const timeElement = document.getElementById("time-value");
       if (timeElement) {
         timeElement.textContent = currentTime;
-      }
-
-      const timeElementCell = document.getElementById("time-value-cell");
-      if (timeElementCell) {
-        timeElementCell.textContent = currentTime;
       }
 
       const utcOffsetElement = document.getElementById("utcoffset-value");
@@ -75,8 +60,8 @@ async function setCurrentTime() {
         utcOffsetElement.textContent = utcOffset?.toString() ?? "-";
       }
 
+      logMessage("Querying for current temperature and humidity");
       const sensorData = await bleDevice.querySensor();
-      console.log(sensorData.humidity);
       const unit = await bleDevice.getUnits();
       const tempElement = document.getElementById("temperature-value");
       if (tempElement) {
@@ -88,17 +73,23 @@ async function setCurrentTime() {
         humidityElement.textContent = `${sensorData.humidity}%`;
       }
     } catch (error) {
-      console.error(error);
+      logMessage(error);
       if (error instanceof Error) {
-        statusElement.textContent = `${error.name}: ${error.message}`;
-        logInfo(error);
+        logMessage(`${error.name}: ${error.message}`);
       }
     } finally {
-      await delay(20000);
       bleDevice.cleanup();
-      statusElement.textContent = "Done.";
+      logMessage("Disconnecting from device");
     }
-  });
+  }
+}
+
+(async () => {
+  const setTimeButton = document.getElementById("set-time-button");
+  setTimeButton?.addEventListener("click", () => setCurrentTime());
+
+  const button = document.getElementById("ble-button");
+  button?.addEventListener("click", () => queryDevice());
 })();
 
 function delay(ms: number) {
